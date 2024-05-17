b3e77ca1a3fcee91325a9137f7eabba4af01d4ac
SeokYeong Jeong <aswe0409@gmail.com>
2024-05-17 11:41:01 +0900

    attraction all done

--- backend/src/main/java/com/example/trip/attraction/dao/attractionDAO.java
+++ backend/src/main/java/com/example/trip/attraction/dao/attractionDAO.java
@@ -1,8 +1,10 @@
 package com.example.trip.attraction.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.trip.attraction.dto.AttractionInfoDTO;
 
@@ -10,6 +12,10 @@ import com.example.trip.attraction.dto.AttractionInfoDTO;
 public interface attractionDAO {
 	public List<AttractionInfoDTO> selectList(int cidtype); // 관광지 1개 가져오기
 	public List<AttractionInfoDTO> getRandomAttractions(String region); //관광지 랜덤 3개 가져오기
-	
-	
+	//범위내 관광지 조회
+	public List<Map<String, Object>> getInRangeAttractions(@Param("latitude") double latitude, @Param("longitude") double longitude, @Param("radius") double radius);
+	// 음식집 랜덤 3개 가져오기
+	public List<Map<String, Object>> getRandomRestaurant(String region);
+	//조회순 top3 보여주기
+	public List<Map<String, Object>> getTop3(String region);
 }
--- backend/src/main/java/com/example/trip/util/WebConfig.java
+++ backend/src/main/java/com/example/trip/util/WebConfig.java
@@ -1,15 +1,18 @@
-package com.example.trip.util;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-				.allowedHeaders("*").allowCredentials(true);
-	}
-}
+//package com.example.trip.util;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")  // 프론트엔드 도메인을 명시적으로 설정
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
